c0bc8878b00c19b26459462fb9a633242e96f0c1
Harsh Ajudia <harshajudia99@gmail.com>
2020-01-08 09:24:27 +0530

    user service
    user service

--- src/app/component/users/get-users/get-users.component.ts
+++ src/app/component/users/get-users/get-users.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { UsersSchema, GetUsersService } from '../../../services/users/get-users.service';
+import { GetUsersService } from '../../../services/users/get-users.service';
 @Component({
   selector: 'app-get-users',
   templateUrl: './get-users.component.html',
   styleUrls: ['./get-users.component.css']
 })
 export class GetUsersComponent implements OnInit {
-  public usersData = [];
+  public usersData = {};
   constructor(private _getUsersService: GetUsersService) {
     this.getUsers();
   }
@@ -15,8 +15,9 @@ export class GetUsersComponent implements OnInit {
 
   }
   getUsers() {
-    this._getUsersService.getUsers().subscribe(res => {
+    this._getUsersService.getUsers().then((res) => {
       this.usersData = res;
+      console.log(res);
     });
   }
 }
--- src/app/services/users/get-users.service.ts
+++ src/app/services/users/get-users.service.ts
@@ -1,22 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class GetUsersService {
   public getUserUrl = "http://localhost:3000/users";
   constructor(private _httpClient: HttpClient) { }
-  
-  subscriptionUsers: UsersSchema[] = [];
-  getUsers() {
 
-    return this._httpClient.get(this.getUserUrl);
-    
+  getUsers() {
+    let promise = new Promise((resolve, reject) => {
+      this._httpClient.get(this.getUserUrl)
+          .toPromise()
+          .then(
+            res => {
+              let response = res;
+              resolve();
+            },
+            msg => {
+              reject(msg);
+            }
+          )
+    });
+    return promise;
   }
 }
-export class UsersSchema {
+/* export class UsersSchema {
   public id: number;
   public full_name: string;
   public email: string;
-}
+} */
